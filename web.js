50f648ef754f3918205b186b767b14006417ffc1
Rafa Shobr <raphi.schobi@gmail.com>
2013-07-11 00:29:39 +0100

    new version of web.js

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var readAndSendFileContent = function (file, respond) {
+	fs.readFile(file, 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err);
+		}
+	respond(data);
+	});
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  readAndSendFileContent("index.html", response.send);
 });
 
 var port = process.env.PORT || 5000;

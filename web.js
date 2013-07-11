ec805ee3352d91f2fbd4872eb51564b418a5c8f6
Rafa Shobr <raphi.schobi@gmail.com>
2013-07-11 01:38:06 +0100

    "rename" old repo to bitstarter

---
+++ web.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+function readAndSendFileContent(file, response) {
+	fs.readFile(file, 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err);
+		}
+	response.send(data);
+	});
+}
+
+app.get('/', function(request, response) {
+  readAndSendFileContent('./index.html', response);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
